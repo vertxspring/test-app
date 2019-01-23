d774d5a2845bd078e51fca23f29726d73de7deb0
SM <vandaal05@gmail.com>
2019-01-23 10:39:09 +0530

    aa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-app';
+  xyz = "Bye!"
+  abc = false;
+  test(){
+    console.log("Button clicked");
+  }
+
+
+  test2(){
+    console.log("2 Button clicked");
+this.xyz = "qwerty";
+this.abc = true;
+  }
+
+
+
+  test3(){
+    this.xyz = "CHanged!"
+    console.log("3 Button clicked");
+    this.abc = false;
+  }
+
+  test4(){
+    console.log("Blurred!");
+  }
 }
